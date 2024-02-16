79b082c52bb81370ff8e0aedad415a690b5cab23
chaniru04 <chaniru.20231195@iit.ac.lk>
2024-02-16 14:13:41 +0530

    Login page - version 0.1

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.loginpage {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.loginpage to javafx.fxml;
+    exports com.example.loginpage;
+}
